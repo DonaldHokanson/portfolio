a817bca18f3afa87c1db394419b5ee7c6aca8622
Donald Hokanson <donaldhokanson@gmail.com>
2016-05-21 10:41:32 -0500

    added animations to home.js

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,19 +1,55 @@
 var main = function(){
-	$('.about').hover(
 
-	function(){
-		$(this).animate({
-			right: '10px',
-			bottom: '10px'
-		}, 200);
-	},
-
-	function(){
-		$(this).animate({
-			left: '0px',
-			top: '0px'
-		}, 400);
+/* .about */
+	$('.about').mouseenter(function(){
+		$(this).stop(true,true).animate({ "right":"+=10px", "bottom":"+=10px", "opacity":".75"}, 300);
 	});
+	$('.about').mouseleave(function(){
+		$(this).stop(true,true).animate({ "right":"-=10px", "bottom":"-=10px", "opacity":"1"}, 400)
+	});
+
+/* .projects */
+	$('.projects').mouseenter(function(){
+		$(this).stop(true,true).animate({ "left":"+=10px", "bottom":"+=10px", "opacity":".75"}, 300);
+
+	});
+	$('.projects').mouseleave(function(){
+		$(this).stop(true,true).animate({ "left":"-=10px", "bottom":"-=10px", "opacity":"1"}, 400)
+	});
+
+/* .resume */
+	$('.resume').mouseenter(function(){
+		$(this).stop(true,true).animate({ "right":"+=10px", "top":"+=10px", "opacity":".75"}, 300);
+
+	});
+	$('.resume').mouseleave(function(){
+		$(this).stop(true,true).animate({ "right":"-=10px", "top":"-=10px", "opacity":"1"}, 400)
+	});
+
+/* .contact */
+	$('.contact').mouseenter(function(){
+		$(this).stop(true,true).animate({ "left":"+=10px", "top":"+=10px", "opacity":".75"}, 300);
+
+	});
+	$('.contact').mouseleave(function(){
+		$(this).stop(true,true).animate({ "left":"-=10px", "top":"-=10px", "opacity":"1"}, 400)
+	});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 };
 
